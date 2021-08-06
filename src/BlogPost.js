afd8c4d5c0224ea646df5002c9ae7fe29fa67dc5
Adrian Walker <awalker3365@gmail.com>
2021-08-05 23:38:08 -0400

    8-5-21 1138p

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,4 +1,5 @@
 import React from ("react")
+import BlogList from "./BlogList"
 
 function BlogPost(props) {
 
@@ -6,8 +7,8 @@ function BlogPost(props) {
 
     return (
         <div>
-            <h1>1</h1>
-            <h3>2</h3>
+            <h1>{}</h1>
+            <h3>{}</h3>
             <p>3</p>
         </div>
     )
