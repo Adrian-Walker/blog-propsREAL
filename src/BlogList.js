9bc7b9f34029efb8721e39d19cf58998bde79ca1
Adrian Walker <awalker3365@gmail.com>
2021-08-05 23:00:57 -0400

    8-5-21 11p

--- src/BlogList.js
+++ src/BlogList.js
@@ -4,10 +4,10 @@ import BlogPost from "./BlogPost"
 import mapArray from "./mapArray"
 
 function BlogList() {
-    const blogs = mapArray.map(post => { })
+    const blogs = mapArray.map(post => <BlogPost title={array.title} subtitle={array.subtitle} author={array.author} date={array.date} />)
     return (
         <div>
-            <BlogPost />
+            {blogs}
         </div>
     )
 }
